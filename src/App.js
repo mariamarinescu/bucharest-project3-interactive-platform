3408f398f31723b4c932f0a6ccdde87879107a7e
Maria <fmariamarinescu@gmail.com>
2019-12-03 21:37:54 +0200

    creates modal & adds bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import SingUp from './components/signup/SignUp'
 
 function App() {
   return (
     <div className="App">
-     
+     <SingUp/>
     </div>
   );
 }
---
+++ src/components/signup/SignUp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Modal, Button, Form} from 'react-bootstrap';
+
+
+class SignUp extends React.Component {
+    render() {
+        return(
+<div>
+<Modal.Dialog>
+  <Modal.Header closeButton>
+    <Modal.Title>Inregistrare</Modal.Title>
+  </Modal.Header>
+
+  <Modal.Body>
+  
+  </Modal.Body>
+</Modal.Dialog>
+</div>
+        )
+    }
+    
+}
+
+export default SignUp;

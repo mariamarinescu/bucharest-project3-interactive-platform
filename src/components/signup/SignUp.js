cc5064502ded9e68d9d475dd4873d52d4eb23482
Maria <fmariamarinescu@gmail.com>
2019-12-04 13:42:26 +0200

    prepares inputs for handling errors

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import './style.css'
+import validator from 'email-validator'
+import classNames from 'classnames';
 
 
 class SignUp extends React.Component {
@@ -28,33 +30,32 @@ class SignUp extends React.Component {
                     </Modal.Header>
 
                     <Modal.Body>
-                        <form>
-                            <input className='input'
-                                placeholder="Nume complet"
-                                ref={(f) => { this.nameField = f; }}
-                                type="text" />
-                            {fieldNameError}
+                    <form>
+                    <input className={classNames("input", { ["inputError"]: this.state.errorNameText })}
+                        placeholder="Your name"
+                        ref={(f) => { this.nameField = f; }}
+                        type="text" />
+                        {fieldNameError}
 
-                            <input className='input'
-                                placeholder="Adresa de email"
-                                ref={(f) => { this.emailField = f; }}
-                                type="email" />
-                            {fieldEmailError}
-                            <input className='input'
-                                placeholder="Parola"
-                                ref={(f) => { this.passwordField = f; }}
-                                type="password" />
-                            {fieldPasswordError}
-                            <input className='input'
-                                placeholder="Confirma parola"
-                                ref={(f) => { this.confPasswordField = f; }}
-                                type="password" />
-                            {fieldConfirmPassError}
-                            <div className="actionContainer">
-                                <div className="button" onClick={this.login}>Login</div>
-                            </div>
-                        </form>
-
+                    <input className={classNames("input", { ["inputError"]: this.state.errorEmailText })}
+                        placeholder="Your email"
+                        ref={(f) => { this.emailField = f; }}
+                        type="email" />
+                    {fieldEmailError}
+                    <input className={classNames("input", { ["inputError"]: this.state.errorPasswordText })}
+                        placeholder="Your password"
+                        ref={(f) => { this.passwordField = f; }}
+                        type="password" />
+                        {fieldPasswordError}
+                    <input className={classNames("input", { ["inputError"]: this.state.errorConfirmPasswordText })}
+                        placeholder="Confirm password"
+                        ref={(f) => { this.confPasswordField = f; }}
+                        type="password" />
+                        {fieldConfirmPassError}
+                    <div className="actionContainer">
+                        <div className="button" onClick={this.login}>Login</div>
+                    </div>
+                </form>
                     </Modal.Body>
                 </Modal.Dialog>
             </div>

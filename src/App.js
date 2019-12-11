ff3bc37b1bdec33fa93f9671aa973048f0270e54
S0nicTheHedgehog <notyorphobia@gmail.com>
2019-12-11 11:40:02 +0200

    map done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import Maps from './components/maps/Maps'
-import { Container, Row, Col } from 'react-bootstrap';
 
+
+// DUMMY DATA 
+const markerData = [{
+  title: 'marker1_title',
+  name: 'Kathmandu City',
+  position: {
+    lat: 27.708259611420385,
+    lng: 85.31940732779083
+  }
+}, {
+  title: 'marker2_title',
+  name: 'Salta City',
+  position: {
+    lat: -24.795283873195597,
+    lng: -65.42829136976184
+  }
+}, {
+  title: 'marker3_title',
+  name: 'Oracoke Lighthouse',
+  position: {
+    lat: 35.1096357,
+    lng: -75.9863747
+  }
+}, {
+  title: 'marker4_title',
+  name: 'Antananarivo City',
+  position: {
+    lat: -18.915194943870617,
+    lng: 47.53313308528492
+  }
+}, {
+  title: 'marker5_title',
+  name: 'Esports',
+  position: {
+    lat: 37.5246642275535,
+    lng: 126.94545210259305
+  }
+}, {
+  title: 'marker6_title',
+  name: 'Neo-Tokyo',
+  position: {
+    lat: 35.675784883832065,
+    lng: 139.7688509481294
+  }
+}, {
+  title: 'marker7_title',
+  name: 'Boring',
+  position: {
+    lat: 47.44931986261183,
+    lng: 12.154022557394521
+  }
+}]
 
 export default class App extends Component {
   constructor(props) {
@@ -15,12 +64,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>
-          <div style = {{height: '60vh'}}>
-            
-              <Maps />
-            
-          </div>
+        <div style={{ height: '60vh' }}>
+          <Maps locationData={markerData} />
         </div>
       </div>
     );
--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -1,39 +1,57 @@
 import React from 'react'
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper, InfoWindow } from 'google-maps-react';
 
-
-const apiKey = 'AIzaSyBuqivjlUHx_1CzHXli6ft9xLWrI-dOGwo'
-const yek = 'AIzaSyBFBgzKEMmLB-LGdSrmFOejutT8m8pOvQk'
-const coord = {
-    lat: 35.1096357,
-    lng: -75.9863747
-}
+const apiKey = 'AIzaSyBuqivjlUHx_1CzHXli6ft9xLWrI-dOGwo' /* ISS TEAM API KEY */
+// const yek = 'AIzaSyBFBgzKEMmLB-LGdSrmFOejutT8m8pOvQk' /* MY BROKEN API KEY */
 
 class GMap extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        }
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+        };
+        this.onMarkerClick = this.onMarkerClick.bind(this)
+        this.onMapClicked = this.onMapClicked.bind(this)
     }
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+    onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    };
     render() {
         return (
             <div className='map'>
-                    <Map google={this.props.google} zoom={5}
-                        initialCenter={{
-                            lat: 44.452404,
-                            lng: 26.075714599999998
-                        }}
-
-                    >
-                        <Marker
-                            title={'Bucharest'}
-                            name={'Bucharest'}
-                            position={{
-                                lat: 44.452404,
-                                lng: 26.075714599999998
-                            }}
+                <Map google={this.props.google}
+                    zoom={3}
+                // initialCenter={coord}  !!!SET DEFAULT CENTER!!!
+                >
+                    {this.props.locationData.map((e, i) => {
+                        return <Marker
+                            title={e.title}
+                            name={e.name}
+                            position={e.position}
+                            onClick={this.onMarkerClick}
                         />
-                    </Map>
+                    })}
+                    <InfoWindow
+                        marker={this.state.activeMarker}
+                        visible={this.state.showingInfoWindow}>
+                        <div>
+                            <h1>{this.state.selectedPlace.name}</h1>
+                        </div>
+                    </InfoWindow>
+                </Map>
             </div>
         )
     }

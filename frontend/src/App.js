3d89619709f8cfef97511c46cf37ac91d1d6b774
Maria <fmariamarinescu@gmail.com>
2019-12-12 19:30:12 +0200

    path fix

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Quiz from './components/quiz//Quiz'
+import'./components/quiz/scss/quiz.css'
+
+
+function App() {
+  return (
+    <div className="App">
+     <Quiz/>
+    </div>
+  );
+}
+
+export default App;

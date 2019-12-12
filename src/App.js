19fb615f2a0d1b1160905f843a9638d76d5b67d5
S0nicTheHedgehog <notyorphobia@gmail.com>
2019-12-12 12:53:39 +0200

    done sexy gurl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LightningBoye from './components/Gurl/GirlModel'
+import './App.css'
 
 
 export default class App extends Component {
--- src/components/Gurl/GirlModel.js
+++ src/components/Gurl/GirlModel.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import img from '../../Kool_Boye.jpg'
+import img from '../../Kool_Gurl.jpg'
+
+const points = ['arm', 'leg', 'hair', 'face', 'hand', 'neck']
 
 export default class GirlModel extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    handleClick = (event) => {
+        console.log(event)
     }
 
     render() {
         return (
-            <div>
-                <img src={img} alt='Da_ly' />
+            <div className='model'>
+                <div className='gurlContainer'>
+                    <img src={img} alt='model' className='gurl' />
+                    {points.map((e, i) => {
+                        return <div className={'spinner ' + e} key={i} onClick={() => this.handleClick(e)}>
+                            <div className="multi-ripple">
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </div>
+                    })}
+                </div>
             </div>
         );
     }

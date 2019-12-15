d82525eae355c034bdda19bb813bb9f34865e6fd
Maria <fmariamarinescu@gmail.com>
2019-12-15 12:48:59 +0200

    card component & fake db ready

---
+++ frontend/src/components/quiz-card/QCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+class QCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            face : {
+                "q-one": ""
+            }
+        }
+    }
+}

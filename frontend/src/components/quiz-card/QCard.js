461f2948c3677e5b4656ea3e7ffb3c5bd575e1bb
Maria <fmariamarinescu@gmail.com>
2019-12-15 12:56:18 +0200

    setting the state

--- frontend/src/components/quiz-card/QCard.js
+++ frontend/src/components/quiz-card/QCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as data from './my_fake_db.json';
 
 
 
@@ -7,8 +8,69 @@ class QCard extends React.Component {
         super(props);
         this.state = {
             face : {
-                "q-one": ""
+                firstQ: {
+                    question: data.face.firstQuestion.question,
+                    answers : data.face.firstQuestion.answers,
+                    correct: data.face.firstQuestion.correct,
+                    image: data.face.firstQuestion.firstQuestionImg
+                },
+                secondQ: {
+                    question: data.face.secondQuestion.question,
+                    answers : data.face.secondQuestion.answers,
+                    correct: data.face.secondQuestion.correct,
+                    image: data.face.secondQuestion.secondQuestionImg
+                }
+                
+            },
+            hair : {
+                firstQ: {
+                    question: data.hair.firstQuestion.question,
+                    answers : data.hair.firstQuestion.answers,
+                    correct: data.hair.firstQuestion.correct,
+                    image: data.hair.firstQuestion.firstQuestionImg
+                },
+                secondQ: {
+                    question: data.hair.secondQuestion.question,
+                    answers : data.hair.secondQuestion.answers,
+                    correct: data.hair.secondQuestion.correct,
+                    image: data.hair.secondQuestion.secondQuestionImg
+                }
+                
+            },
+            massage : {
+                firstQ: {
+                    question: data.massage.firstQuestion.question,
+                    answers : data.massage.firstQuestion.answers,
+                    correct: data.massage.firstQuestion.correct,
+                    image: data.massage.firstQuestion.firstQuestionImg
+                },
+                secondQ: {
+                    question: data.massage.secondQuestion.question,
+                    answers : data.massage.secondQuestion.answers,
+                    correct: data.massage.secondQuestion.correct,
+                    image: data.massage.secondQuestion.secondQuestionImg
+                }
+                
+            },
+            anticelulitis : {
+                firstQ: {
+                    question: data.anticelulitis.firstQuestion.question,
+                    answers : data.anticelulitis.firstQuestion.answers,
+                    correct: data.anticelulitis.firstQuestion.correct,
+                    image: data.anticelulitis.firstQuestion.firstQuestionImg
+                },
+                secondQ: {
+                    question: data.anticelulitis.secondQuestion.question,
+                    answers : data.anticelulitis.secondQuestion.answers,
+                    correct: data.anticelulitis.secondQuestion.correct,
+                    image: data.anticelulitis.secondQuestion.secondQuestionImg
+                }
+                
             }
         }
     }
+
+//    retrieveData = () => {
+
+//     }
 }

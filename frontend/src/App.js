9c3c62a0d5303f9d116677f0bedf125fec236068
Maria <fmariamarinescu@gmail.com>
2019-12-15 13:18:34 +0200

    creates bootstrap-card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import QCard from './components/quiz-card/QCard'
 
 function App() {
   return (
     <div className="App">
-     
+     <QCard/>
     </div>
   );
 }

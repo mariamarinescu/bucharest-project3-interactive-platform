a4bfc349879ff9bd8bb583d346a975e560b912ee
Maria <fmariamarinescu@gmail.com>
2019-12-16 16:57:53 +0200

    folder structure fix-prepares brach for merge

--- src/pages/Contact/ContactPage.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react";
-
-const ContactPage = () => (
-    <Fragment>
-        <h1>Contact</h1>
-    </Fragment>
-);
-
-export default ContactPage;
--- src/pages/Home/HomePage.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react";
-
-const HomePage = () => (
-    <Fragment>
-        <h1>Home</h1>
-    </Fragment>
-);
-
-export default HomePage;

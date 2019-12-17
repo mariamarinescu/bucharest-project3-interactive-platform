875f6b56ade617d491e03f7430ab2a6cb231da4a
S0nicTheHedgehog <notyorphobia@gmail.com>
2019-12-17 16:32:01 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route path='/users'>
+
+            </Route>
+            <Route path='/admin'>
+
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+
+

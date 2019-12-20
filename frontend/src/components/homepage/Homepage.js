86dfe546fc83a263acd676ffc58748dc9e85c8ec
Maria <fmariamarinescu@gmail.com>
2019-12-20 02:38:59 +0200

    everything imported & it works

--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route, withRouter, NavLink, Link } from 'react-router-dom';
-import { Button, Modal, Row, Col, Form } from 'react-bootstrap';
 
 
 import data from '../info_modal/my_fake_db.json';
@@ -11,8 +10,8 @@ import Nav from '../navbar/NavBar';
 import Quiz from '../quiz/Quiz';
 import Footer from '../footer/Footer';
 import GirlModel from '../Gurl/GirlModel';
-
-
+import Parteners from '../parteners/Parteners';
+import SignUp from '../signup/SignUp';
 
 import '../parteners/Parteners.css'
 
@@ -31,30 +30,6 @@ import '../quiz/quiz.css'
 
 
 
-
-const FormErrors = ({ formErrors }) =>
-    <div className='formErrors'>
-        {Object.keys(formErrors).map((fieldName, i) => {
-            if (formErrors[fieldName].length > 0) {
-                if (fieldName === 'email') {
-                    return (
-                        <p key={i}>{'Email-ul'} {formErrors[fieldName]}</p>
-                    )
-                } else if (fieldName === 'password') {
-                    return (
-                        <p key={i}>{'Parola'} {formErrors[fieldName]}</p>
-                    )
-                } else if (fieldName === 'confirmPassword') {
-                    return (
-                        <p key={i}>{'Parolele'} {formErrors[fieldName]}</p>
-                    )
-                }
-
-            } else {
-                return '';
-            }
-        })}
-    </div>;
 
 
 
@@ -110,186 +85,6 @@ const markerData = [
         }
     }];
 
-
-
-
-class Parteners extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            megaImage: data.quiz.parteners.megaImage,
-            carturesti: data.quiz.parteners.carturesti,
-            farmaciaVital: data.quiz.parteners.farmaciaVital,
-            eNatural: data.quiz.parteners.eNatural,
-            camaraCuMerinde: data.quiz.parteners.camaraCuMerinde,
-            stamDeVorba: data.quiz.parteners.stamDeVorba,
-            allBoutique: data.quiz.parteners.allBoutique,
-            tastingRomania: data.quiz.parteners.tastingRomania,
-            iCosmetice: data.quiz.parteners.iCosmetice,
-            elaPlant: data.quiz.parteners.elaPlant,
-            byaPlantMed: data.quiz.parteners.byaPlantMed,
-            floraFarm: data.quiz.parteners.floraFarm,
-            redPixie: data.quiz.parteners.redPixie,
-            getWell: data.quiz.parteners.getWell,
-            natura4All: data.quiz.parteners.natura4All,
-            avalon: data.quiz.parteners.avalon
-
-
-        }
-    }
-
-
-    // retrieveParteners = () => {
-    // }
-
-
-
-    render() {
-        return (
-            <div className="parteners-container" >
-                <h1 className="p-title">Parteneri Online</h1>
-                <img className="p-mega" src={this.state.megaImage} alt="Mega" />
-                <img className="p-carturesti" src={this.state.carturesti} alt="Carturesti" />
-                <img className="p-vital" src={this.state.farmaciaVital} alt="farmacia-Vital" />
-                <img className="p-enatural" src={this.state.eNatural} alt="e-Natural" />
-                <img className="p-camara" src={this.state.camaraCuMerinde} alt="Camara-Cu-Merinde" />
-                <img className="p-stam-de-vorba" src={this.state.stamDeVorba} alt="Stam-De-Vorba" />
-                <img className="p-allboutique" src={this.state.allBoutique} alt="allBoutique" />
-                <img className="p-tastingRO" src={this.state.tastingRomania} alt="Tasting-Romania" />
-                <img className="p-icosmetice" src={this.state.iCosmetice} alt="iCosmetice" />
-                <img className="p-ela" src={this.state.elaPlant} alt="ElaPlant" />
-                <img className="p-bya" src={this.state.byaPlantMed} alt="ByaPlantMed" />
-                <img className="p-flora" src={this.state.floraFarm} alt="FloraFarm" />
-                <img className="p-pixie" src={this.state.redPixie} alt="RedPixie" />
-                <img className="p-get-well" src={this.state.getWell} alt="GetWell" />
-                <img className="p-natura" src={this.state.natura4All} alt="Natura4All" />
-                <img className="p-avalon" src={this.state.avalon} alt="Avalon" />
-            </div>
-        )
-    }
-}
-
-
-
-
- class SignUp extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-            formErrors: { email: '', password: '', confirmPassword: '' },
-            emailValid: false,
-            passwordValid: false,
-            formValid: false,
-            confirmPasswordValid: false
-        }
-
-    }
-
-
-    handleUserInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({ [name]: value },
-            () => { this.validateField(name, value) })
-        if (this.state.password === this.state.confirmPassword && this.state.password !== '' && this.state.confirmPassword !== '') {
-            this.setState({
-                confirmPasswordValid: true
-            })
-        }
-
-    }
-
-
-    validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.formErrors;
-        let emailValid = this.state.emailValid;
-        let passwordValid = this.state.passwordValid;
-        let confirmPasswordValid = this.state.confirmPasswordValid;
-
-        switch (fieldName) {
-            case 'email':
-                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i); //an example regex from the Devise library.
-                fieldValidationErrors.email = emailValid ? '' : ' nu este valid.';
-                break;
-            case 'password':
-                passwordValid = value.length >= 6;
-                fieldValidationErrors.password = passwordValid ? '' : ' este prea scurta.';
-                break;
-            case 'confirmPassword':
-                confirmPasswordValid = value === this.state.password;
-                fieldValidationErrors.confirmPassword = confirmPasswordValid ? '' : 'nu se potrivesc.';
-                break;
-            default:
-                break;
-
-        }
-
-        this.setState({
-            formErrors: fieldValidationErrors,
-            emailValid: emailValid,
-            passwordValid: passwordValid
-        }, this.validateForm);
-    }
-    routeChange = () => {
-        let path = `/`;
-        this.props.history.push(path);
-    }
-
-    validateForm() {
-        this.setState({ formValid: this.state.emailValid && this.state.passwordValid });
-    }
-
-    render() {
-        return (
-            <Row className="signup-form">
-                <Col className='py-3' md={{ offset: 8, span: 3 }} xs={{ offset: 4, span: 7 }}>                <form className="">
-                    <div className="form-group">
-                        <input type="text" className="form-control mySignUp-input"
-                            name="name" value={this.state.name}
-                            placeholder="Nume complet"
-                            onChange={(event) => this.handleUserInput(event)} />
-                    </div>
-                    <div className="form-group">
-                        <input type="email" className="form-control mySignUp-input"
-                            name="email" value={this.state.email}
-                            placeholder="Email"
-                            onChange={(event) => this.handleUserInput(event)} />
-                    </div>
-                    <div className="form-group">
-                        <input type="password" className="form-control mySignUp-input"
-                            name="password" value={this.state.password}
-                            placeholder="Parola"
-                            onChange={(event) => this.handleUserInput(event)} />
-                    </div>
-                    <div className="form-group">
-                        <input type="password" className="form-control mySignUp-input"
-                            name="confirmPassword" value={this.state.confirmPassword}
-                            placeholder="Confirma parola"
-                            onChange={(event) => this.handleUserInput(event)} />
-                    </div>
-                    <Col>
-                        <Button variant="outline-dark" className="mySignUp-bttn" type="submit"
-                            disabled={!this.state.formValid} onClick={this.routeChange}>
-                            Creeare cont
-                    </Button></Col>
-                </form>
-                    <div className="panel panel-default">
-                        <FormErrors formErrors={this.state.formErrors} />
-                    </div>
-                </Col></Row>
-        )
-    }
-}
-
-
-
-
-
 class Homepage extends React.Component {
     render() {
         return (
@@ -298,7 +93,8 @@ class Homepage extends React.Component {
                     <Nav />
                     <GirlModel />
                     <div style={{ height: '60vh' }}>
-                        <Maps locationData={markerData} />                    </div>
+                        <Maps locationData={markerData} />                    
+                        </div>
                     <Parteners />
                     <Footer />
                     <Switch>

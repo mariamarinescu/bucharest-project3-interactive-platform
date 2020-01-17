d8cd32ebf7871378d09f1fd96735443a8fbfc08e
rriamarria <fmariamarinescu@gmail.com>
2020-01-17 10:58:50 +0200

    Updates

---
+++ precache-manifest.bde20f6fb71b2150d00aab8e9eb10c55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f92a1abc29fe015d6fd8ee524edbd00",
+    "url": "/bucharest-project3-interactive-platform/index.html"
+  },
+  {
+    "revision": "321778681a88f8779166",
+    "url": "/bucharest-project3-interactive-platform/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c34ec86f5865d2c5a7b9",
+    "url": "/bucharest-project3-interactive-platform/static/css/main.b2a3f16b.chunk.css"
+  },
+  {
+    "revision": "321778681a88f8779166",
+    "url": "/bucharest-project3-interactive-platform/static/js/2.be54aaaf.chunk.js"
+  },
+  {
+    "revision": "d342664d578042145f5010e743958e89",
+    "url": "/bucharest-project3-interactive-platform/static/js/2.be54aaaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c34ec86f5865d2c5a7b9",
+    "url": "/bucharest-project3-interactive-platform/static/js/main.c4876f1d.chunk.js"
+  },
+  {
+    "revision": "effed892af4dc9a049a9",
+    "url": "/bucharest-project3-interactive-platform/static/js/runtime-main.93642916.js"
+  },
+  {
+    "revision": "945ed563be641e46207dd658ad048cea",
+    "url": "/bucharest-project3-interactive-platform/static/media/Kool_Gurl.945ed563.jpg"
+  },
+  {
+    "revision": "930c2b0f93e999482e6b8e2b8aa8e24e",
+    "url": "/bucharest-project3-interactive-platform/static/media/brand-romanesc.930c2b0f.png"
+  },
+  {
+    "revision": "3c8da8781995ffc90e5a793bb10ce294",
+    "url": "/bucharest-project3-interactive-platform/static/media/hook-1425312.3c8da878.png"
+  },
+  {
+    "revision": "21b15624ecc5a3bbcdf8913111313171",
+    "url": "/bucharest-project3-interactive-platform/static/media/logo_techir.21b15624.png"
+  },
+  {
+    "revision": "54e8ba365f0c80ecf8addb2e66e751b6",
+    "url": "/bucharest-project3-interactive-platform/static/media/produse-romanesti.54e8ba36.png"
+  }
+]);
